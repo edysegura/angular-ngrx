9e1656d7e669580a49d64fbe2cb7531a7e12121b
Edy Segura <edysegura@gmail.com>
2019-02-06 23:08:06 -0200

    Add config actions

---
+++ src/app/store/actions/config.actions.ts
@@ -0,0 +1,19 @@
+import { Action } from '@ngrx/store';
+
+import { Config } from '../../models/config.model';
+
+export enum ConfigActionsList {
+  GetConfig = '[Config] Get Config',
+  GetConfigSuccess = '[Config] Get Config Success'
+}
+
+export class GetConfig implements Action {
+  readonly type = ConfigActionsList.GetConfig;
+}
+
+export class GetConfigSuccess implements Action {
+  readonly type = ConfigActionsList.GetConfigSuccess;
+  constructor(public payload: Config[]) {}
+}
+
+export type ConfigAction = GetConfig | GetConfigSuccess;
