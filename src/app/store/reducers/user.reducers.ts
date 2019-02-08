5042b705aaaff13c380c7f198b09e7795756e557
Edy Segura <edysegura@gmail.com>
2019-02-08 21:31:20 -0200

    Add user reducers

---
+++ src/app/store/reducers/user.reducers.ts
@@ -0,0 +1,27 @@
+import { UserActionsList } from '../actions/user.actions';
+import { UserActions } from '../actions/user.actions';
+import { initialUserState, UserState } from '../state/user.state';
+
+export const userReducers = (
+  state = initialUserState,
+  action: UserActions
+): UserState => {
+  switch(action.type) {
+    case UserActionsList.GetUsersSuccess: {
+      return {
+        ...state,
+        users: action.payload
+      };
+    }
+
+    case UserActionsList.GetUserSuccess: {
+      return {
+        ...state,
+        selectedUser: action.payload
+      };
+    }
+
+    default:
+      return state;
+  }
+}
