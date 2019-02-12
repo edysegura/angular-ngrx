8f8695b6a3656eff43f5faf7d35bb458bdfa3ebd
Edy Segura <edysegura@gmail.com>
2019-02-12 16:48:00 -0200

    Add config.reducers.ts

---
+++ src/app/store/reducers/config.reducers.ts
@@ -0,0 +1,18 @@
+import { ConfigActions, ConfigActionsList } from '../actions/config.actions';
+import { initialConfigState, ConfigState } from '../state/config.state';
+
+export const configReducers = (
+  state = initialConfigState,
+  action: ConfigActions
+): ConfigState => {
+  switch (action.type) {
+    case ConfigActionsList.GetConfigSuccess: {
+      return {
+        ...state,
+        config: action.payload
+      };
+    }
+    default:
+      return state;
+  }
+};
