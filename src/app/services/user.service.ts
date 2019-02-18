d4824e6d9a25150d6aca18b84fce36c9f9e8c3ae
Edy Segura <edysegura@gmail.com>
2019-02-18 14:29:34 -0300

    Refactor User Service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,14 +1,17 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
+import { environment } from '../../environments/environment';
+import { Users } from '../models/user.model';
 
 @Injectable()
 export class UserService {
-  usersEndpoint = '';
+  private usersEndpoint = `${environment.apiBaseUrl}/users.json`;
 
   constructor(private http: HttpClient) {}
 
-  getUsers() {
-
+  getUsers(): Observable<Users> {
+    return this.http.get<Users>(this.usersEndpoint);
   }
 }
