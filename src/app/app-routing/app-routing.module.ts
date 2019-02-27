1df057750153f2cf0790d6d03b0529db221b9827
Edy Segura <edysegura@gmail.com>
2019-02-26 22:43:55 -0300

    Add router module baseline

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule]
+})
+export class AppRoutingModule {}
