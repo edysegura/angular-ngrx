e32fe820159ad6e1bf4f3a08a2b1fe4d893dfe64
Edy Segura <edysegura@gmail.com>
2019-02-28 13:57:50 -0300

    Update app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,35 @@ import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './containers/user/user.component';
+import { UsersComponent as UsersContainerComponent } from './containers/users/users.component';
+import { UserService } from './services/user.service';
+import { ConfigEffects } from './store/effects/config.effects';
+import { UserEffects } from './store/effects/user.effects';
 import { appReducers } from './store/reducers/app.reducers';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    UsersContainerComponent,
+    UserComponent,
+    UserDetailsComponent,
+    UsersComponent
+  ],
   imports: [
     BrowserModule,
     HttpClient,
     StoreModule.forRoot(appReducers),
-    EffectsModule,
+    EffectsModule.forRoot([ UserEffects, ConfigEffects ]),
     StoreRouterConnectingModule.forRoot({ stateKey: 'router' }),
-    !environment.production ? StoreDevtoolsModule.instrument() : []
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ UserService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule {}
