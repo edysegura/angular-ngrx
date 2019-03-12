7cfa0edfd1dcdc2482ef8364a617021ea8e9a47b
Edy Segura <edysegura@gmail.com>
2019-03-11 22:22:27 -0300

    Implement user container

--- src/app/containers/user/user.component.ts
+++ src/app/containers/user/user.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+
+import { GetUser } from '../../store/actions/user.actions';
+import { AppState } from '../../store/state/app.state';
 
 @Component({
-  selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private store: Store<AppState>,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    const userId = this.route.snapshot.params.id;
+    this.store.dispatch(new GetUser(userId))
   }
 
 }
